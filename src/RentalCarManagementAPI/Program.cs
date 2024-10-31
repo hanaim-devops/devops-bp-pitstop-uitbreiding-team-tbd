27ded2b1c0cef6674b285b80f11aabdf2d696ca0
kriebslang <93926551+kriebslang@users.noreply.github.com>
2024-10-31 23:15:05 +0100

    Automatically run migrations on startup

--- src/RentalCarManagementAPI/Program.cs
+++ src/RentalCarManagementAPI/Program.cs
@@ -41,5 +41,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<RentalCarManagementDBContext>();
+    dbContext.Database.Migrate();
+}
+
 app.MapControllers();
 app.Run();
